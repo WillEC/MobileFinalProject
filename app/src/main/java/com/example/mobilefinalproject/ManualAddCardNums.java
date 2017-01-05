587262b006cc1a4c45685e751c3deedb9385a6bc
WillEC <willchen123456789@gmail.com>
2017-01-05 10:12:31 -0500

    uhhh

---
+++ app/src/main/java/com/example/mobilefinalproject/ManualAddCardNums.java
@@ -0,0 +1,13 @@
+package com.example.mobilefinalproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ManualAddCardNums extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_card_nums);
+    }
+}
---
+++ app/src/main/java/com/example/mobilefinalproject/ManualAddCardSuits.java
@@ -0,0 +1,13 @@
+package com.example.mobilefinalproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ManualAddCardSuits extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_card_suits);
+    }
+}
